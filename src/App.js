29235b3915ab89b8d7613b707c594f6a669a7305
Uchchash346 <mru416@gmail.com>
2022-06-25 20:02:29 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CounterView from './features/counter/CounterView';
 
 function App() {
   return (
     <div className="App">
       <h1>Counter App by Redux Toolkit</h1>
+      <CounterView />
     </div>
   );
 }
